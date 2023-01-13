bfdc01910cc548442bdba3e5cf48c83f820cf67c
dgutper960 <115797153+dgutper960@users.noreply.github.com>
2023-01-13 13:58:28 +0100

    Inicio de las Arrays

---
+++ src/tema_4/arrays/CrearArray.java
@@ -0,0 +1,14 @@
+package tema_4.arrays;
+
+public class CrearArray {
+    public static void main(String[] args) {
+
+        int[] a = {1, 2, 3, 4, 5};
+        System.out.println(a);//Esto imprime la posicion de memoria del objeto
+        System.out.println(a[0]);//Esto imprime el valor de la posicion 0, que es = 1
+        System.out.println(a[1]);//Esto imprime el valor de la posicion 1, que es = 2
+        System.out.println(a[2]);//Esto imprime el valor de la posicion 2, que es = 3
+        System.out.println(a[3]);//Esto imprime el valor de la posicion 3, que es = 4
+        System.out.println(a[4]);//Esto imprime el valor de la posicion 4, que es = 5
+    }
+}
