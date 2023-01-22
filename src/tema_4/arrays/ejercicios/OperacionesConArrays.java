e6a3edcad5f345c340476785bec4b76491afa399
dgutper960 <dgutper960@outlook.es>
2023-01-22 23:12:46 +0100

    Crea Clase OperacionesConArrays 22/01/23.

---
+++ src/tema_4/arrays/ejercicios/OperacionesConArrays.java
@@ -0,0 +1,8 @@
+package tema_4.arrays.ejercicios;
+/** Crea un array de 10 posiciones donde cada posición tenga la tabla de multiplicar de esa posición,
+ * es decir, otro array de 3 posiciones para almacenar el primer operando, el segundo y el resultado. */
+public class OperacionesConArrays {
+    public static void main(String[] args) {
+
+    }
+}
